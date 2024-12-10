d5358f0eda371a5fd85d79775f0f2165255c3e31
An041003 <133753462+An041003@users.noreply.github.com>
2024-12-11 01:03:40 +0700

    match

--- model/match.c
+++ model/match.c
@@ -6,11 +6,22 @@
 #include "../protocol.h"
 #include "auth.h"
 #include "match.h"
+#include <pthread.h>
 
 #define MAX_PLAYERS 10
 Player waitingList[MAX_PLAYERS];
 int waitingCount = 0;
+
+
 void addPlayerToQueue(Player player) {
+
+    for (int i = 0; i < waitingCount; i++) {
+        if (strcmp(waitingList[i].username, player.username) == 0) {
+            printf("Player %s is already in the queue.\n", player.username);
+            return;
+        }
+    }
+
     if (waitingCount < MAX_PLAYERS) {
         waitingList[waitingCount++] = player;
         printf("Player %s added to queue.\n", player.username);
@@ -24,7 +35,8 @@ Player* findOpponent(Player player) {
     int candidateCount = 0;
 
     for (int i = 0; i < waitingCount; i++) {
-        if (strcmp(waitingList[i].username, player.username) != 0 && 
+        if (waitingList[i].username != NULL &&
+            strcmp(waitingList[i].username, player.username) != 0 && 
             abs(waitingList[i].elo - player.elo) <= 100) {
             candidateIndices[candidateCount++] = i;
         }
@@ -65,20 +77,32 @@ void handleMatchmaking(int client_socket, Player player) {
             snprintf(response, BUFFER_SIZE, "MATCH_FOUND %s %d", opponent->username, opponent->elo);
             send(client_socket, response, strlen(response), 0);
             // Xóa cả hai người chơi khỏi hàng chờ
+            // removePlayerFromQueue(&player);
             removePlayerFromQueue(opponent);
-
-            printf("Match found: %s vs %s\n", player.username, opponent->username);
+            MYSQL *conn = connect_database();
+            if (conn == NULL) {
+            close(client_socket);
+            return NULL;
+            }
+            int matchID = create_match(conn, get_player_id(player.username, conn), get_player_id(opponent->username, conn));
+            if (matchID != -1)
+            printf("Match %d: %s vs %s\n",matchID, player.username, opponent->username);
+            mysql_close;
             break; // Thoát khỏi vòng lặp khi tìm được trận
         } else {
             // Nếu không tìm thấy đối thủ, giữ người chơi trong hàng chờ
             printf("No match found yet for %s, waiting...\n", player.username);
             sleep(5); // Đợi 5 giây trước khi kiểm tra lại
         }
+        
     }
 }
 
 //hàm này tạo một trận đấu trong databases và trả về id của trận đó
 int create_match(MYSQL *conn, int playerId1, int playerId2){
+    if (playerId1 == playerId2) {
+        return -1; 
+    }
     char query[256];
     snprintf(query, sizeof(query), "INSERT INTO matches (playerId1, playerId2) VALUES (%d, %d) ",playerId1, playerId2 );
     if (mysql_query(conn, query)) {
