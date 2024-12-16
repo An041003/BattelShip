8fd856b1b3b2ddf5e61af4c6c312c8b51cbc4bf8
tranphuong1205 <phuongxxx971022@gmail.com>
2024-12-16 14:51:44 +0700

    feature: add match control function for server

--- build/CMakeFiles/Server.dir/model/board.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/Server.dir/model/board.c.o: \
- /home/phuong/code/LTM/final_project/battleShip/BattelShip/model/board.c \
- /usr/include/stdc-predef.h \
- /home/phuong/code/LTM/final_project/battleShip/BattelShip/model/board.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdbool.h
